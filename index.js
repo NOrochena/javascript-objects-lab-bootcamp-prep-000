1527d99ef1a00ab1aebb69128326e3ffbb7c52b9
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-26 01:23:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(obj, key){
   return newObj
 }
 
-function destructivelyDeleteFromObjectsByKey(obj, key){
+function destructivelyDeleteFromObjectByKey(obj, key){
   delete obj[key]
   return obj
 }
